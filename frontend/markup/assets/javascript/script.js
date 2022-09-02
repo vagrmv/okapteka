a10ca42d2ecc73b834c3ede9a184c468b2b7a6e5
Vladimir Gromov <v.a.grmv@gmail.com>
2022-09-02 22:58:39 +0500

    added proto nav menu

--- frontend/markup/assets/javascript/script.js
+++ frontend/markup/assets/javascript/script.js
@@ -4,3 +4,18 @@ navToggler.addEventListener("click", function(e){
     e.preventDefault();
     navCollapse.classList.toggle("active");
 })
+
+const navCategoriesItems = document.querySelectorAll(".nav__categories-item > a")
+for (let navCategoriesItem of navCategoriesItems){
+    navCategoriesItem.addEventListener("click", function(e){
+        console.log(navCategoriesItem);
+        e.preventDefault();
+
+        for (let item of navCategoriesItems){
+            console.log("removed")
+            item.classList.remove("active");
+        }
+
+        navCategoriesItem.classList.add("active");
+    });
+}
